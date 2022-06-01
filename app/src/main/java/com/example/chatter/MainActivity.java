83ca8544b1b5cde68919082002f2d692b73dd8c9
Dor Levi <dorlev1204@gmail.com>
2022-06-01 10:39:22 +0300

    login and register activities

--- app/src/main/java/com/example/chatter/MainActivity.java
+++ app/src/main/java/com/example/chatter/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.chatter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.chatter.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.moveToRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
