6526fc22c1ae72109d09895d57299723814df558
ordrukman <92392940+ordrukman@users.noreply.github.com>
2022-06-02 17:49:47 +0300

    add chat page and add contact page

---
+++ app/src/main/java/com/example/chatter/AddContactActivity.java
@@ -0,0 +1,13 @@
+package com.example.chatter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddContactActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact);
+    }
+}
